f1d373f1208b97165302d68027a11c3fa0b2f82c
Vadim Ananiev <vadim12ananev@gmail.com>
2022-11-29 17:33:37 +0200

    close dev

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,4 +1,15 @@
 <?php
+$valid_passwords = array ("super" => "mario");
+$valid_users = array_keys($valid_passwords);
+$user = $_SERVER['PHP_AUTH_USER'];
+$pass = $_SERVER['PHP_AUTH_PW'];
+$validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
+if (!$validated) {
+  header('WWW-Authenticate: Basic realm="My Realm"');
+  header('HTTP/1.0 401 Unauthorized');
+  die ("Not authorized");
+}
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 function dd($arr){
